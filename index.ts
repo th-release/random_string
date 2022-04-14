dc87c21e1eeacb02876522ab41e49cd4d75d5169
Th-release.cth <84012697+th-release@users.noreply.github.com>
2022-04-14 17:50:56 +0900

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,33 @@
+function getRandom (type_: string, length: number) {
+	let result = "";
+	let type = type_.toLowerCase();
+	length = length ? length : 32;
+	let characters = "0123456789";
+	if (type === 'number' || type === 'numbers') {
+		characters = "0123456789";
+		for (var i = 0; i < length; i++) {
+			result += characters[Math.floor(Math.random() * characters.length)];
+		}
+		return result;
+	} else if (type === 'alphabet' || type === 'alphabets') { 
+		characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		for (var i = 0; i < length; i++) {
+			result += characters[Math.floor(Math.random() * characters.length)];
+		}
+		return result;
+	} else if (type === 'alphanumeric' || type === 'all') {
+		characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		for (var i = 0; i < length; i++) {
+			result += characters[Math.floor(Math.random() * characters.length)];
+		}
+		return result;
+	} else {
+		characters = type;
+		for (var i = 0; i < length; i++) {
+			result += characters[Math.floor(Math.random() * characters.length)];
+		}
+		return result;
+	}
+}
+
+export { getRandom };
